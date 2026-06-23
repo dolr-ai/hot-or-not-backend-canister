2cbf3b464fbb17d88bd2e70e8fc97f26b8e0694c
Saikat Das <saikatdas0790@gmail.com>
2026-06-23 18:18:16 +0530

    chore: remove individual user canister management and user record functionality

--- src/canister/user_index/src/api/canister_management/recycle_canisters.rs
+++
@@ -1,9 +0,0 @@
-use ic_cdk::query;
-use shared_utils::canister_specific::user_index::types::RecycleStatus;
-
-use crate::CANISTER_DATA;
-
-#[query]
-pub fn get_recycle_status() -> RecycleStatus {
-    CANISTER_DATA.with_borrow(|canister_data| canister_data.recycle_status.clone())
-}
--- src/canister/user_index/src/util/types/subnet_orchestrator_operation.rs
+++
@@ -1,7 +0,0 @@
-use candid::Principal;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Hash, PartialEq, Eq)]
-pub(crate) enum SubnetOrchestratorOperation {
-    RechargeIndividualUserCanister(Principal),
-}
