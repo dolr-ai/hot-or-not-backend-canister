d42c7904c56e39903d08ec14ab1f396af5ae473f
Saikat Das <saikatdas0790@gmail.com>
2026-06-24 13:13:00 +0530

    chore: move UpgradeStatus definition to platform_orchestrator types

--- src/lib/shared_utils/src/canister_specific/platform_orchestrator/types/mod.rs
+++ src/lib/shared_utils/src/canister_specific/platform_orchestrator/types/mod.rs
@@ -3,10 +3,15 @@ use std::collections::HashMap;
 use candid::{CandidType, Principal};
 use serde::{Deserialize, Serialize};
 
-use crate::canister_specific::user_index::types::UpgradeStatus;
-
 pub mod args;
 pub mod well_known_principal;
+
+#[derive(Default, Clone, CandidType, Serialize, Deserialize)]
+pub struct UpgradeStatus {
+    pub version: String,
+    pub successful_upgrades: u64,
+    pub failed_upgrades: u64,
+}
 
 #[derive(Default, Clone, CandidType, Serialize, Deserialize)]
 pub struct SubnetUpgradeReport {
